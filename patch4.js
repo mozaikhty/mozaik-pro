c2363595408eb8638c4578d345a2148ec3b578fc
mozaikhty <ahmetdincr7@gmail.com>
2026-09-24 18:00:37 +0300

    Delete patch4.js

--- patch4.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('ui.js', 'utf8');
-
-code = code.replace(/document\.getElementById\('inbox-list'\)/g, "document.querySelector('.inbox-list')");
-
-fs.writeFileSync('ui.js', code, 'utf8');
-console.log('Fixed inbox-list selector in ui.js');
