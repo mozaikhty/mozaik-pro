25c6ca370c2ae95155166dde85a833c793352c81
mozaikhty <ahmetdincr7@gmail.com>
2026-09-24 17:56:14 +0300

    Delete fix6.js

--- fix6.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('feed.html', 'utf8');
-
-content = content.replace(/<span>ğŸ” .*?<\/span>/g, '<span>🔍</span>');
-content = content.replace(/onclick=\"window\.sendQuickReaction\('ğŸ‘ .?'\)\">ğŸ‘ .*?<\/span>/g, `onclick="window.sendQuickReaction('👏')">👏</span>`);
-content = content.replace(/ğŸ‘ ï¸ .*?<span id=\"story-view-count\">/g, '👁️ <span id="story-view-count">');
-
-fs.writeFileSync('feed.html', content, 'utf8');
-console.log('Fixed final emojis part 2');
