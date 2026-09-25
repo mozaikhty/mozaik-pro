81764a209b6fa09b4d979014558e856e7dcdc3e7
mozaikhty <ahmetdincr7@gmail.com>
2026-09-25 17:56:04 +0300

    Delete fix_reels2.js

--- fix_reels2.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let s = fs.readFileSync('shared.js', 'utf8');
-
-// Update toggleReelsPlay to strictly Play/Pause
-const oldToggleReelsPlayRegex = /window\.toggleReelsPlay = function\(postId\) \{[\s\S]*?window\.showReelsOverlay[\s\S]*?\}\n\};\n/g;
-
-// Wait, let's just write a script that replaces the entire toggleReelsPlay function
-// using a simpler string replacement if regex is tricky, but let's carefully construct the regex.
