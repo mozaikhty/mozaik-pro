e41f518b5c3c6b207208ae44a3e60f07f8d1a1f9
mozaikhty <ahmetdincr7@gmail.com>
2026-09-26 14:37:28 +0300

    Delete check_close_others.js

--- check_close_others.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-['profile.js', 'feed.js'].forEach(file => {
-    let code = fs.readFileSync(file, 'utf8');
-    const lines = code.split('\n');
-    const idx = lines.findIndex(l => l.includes('window.closePostDetail ='));
-    console.log(`\n--- ${file} ---`);
-    console.log(lines.slice(idx, idx + 8).join('\n'));
-});
