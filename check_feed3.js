d1343c6bda01e711181a3db09d2b7d50472cab29
mozaikhty <ahmetdincr7@gmail.com>
2026-09-26 14:37:52 +0300

    Delete check_feed3.js

--- check_feed3.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('feed.js', 'utf8');
-const lines = code.split('\n');
-const startIdx = lines.findIndex(l => l.includes('window.openPostDetail ='));
-console.log(lines.slice(startIdx + 80, startIdx + 110).join('\n'));
