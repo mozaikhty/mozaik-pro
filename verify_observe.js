92ec28375d0dfbf4b95c30086bb6a2ee61bcf44d
mozaikhty <ahmetdincr7@gmail.com>
2026-09-26 14:40:49 +0300

    Delete verify_observe.js

--- verify_observe.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-['search.js', 'profile.js'].forEach(file => {
-    let code = fs.readFileSync(file, 'utf8');
-    const lines = code.split('\n');
-    console.log(`\n--- ${file} ---`);
-    lines.forEach((l, i) => {
-        if(l.includes('observeModalVideos')) console.log('Line ' + i + ': ' + l);
-    });
-});
