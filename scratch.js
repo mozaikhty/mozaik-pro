7923364cb77dde4a6b9b40af1d5f889c2f00b90b
mozaikhty <ahmetdincr7@gmail.com>
2026-09-26 04:19:56 +0300

    Delete scratch.js

--- scratch.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('feed.html', 'utf8');
-
-// I will move ALL modals outside of app-container, right before new-mozaik-ui.
-// Or actually, if I just remove `display: none !important;` from `.app-container` and instead add `display: none !important;` to `.mozaik-layout`!
-// The modals are direct children of `.app-container` or siblings?
-// Let's check feed.html.
