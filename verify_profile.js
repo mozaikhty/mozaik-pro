b343569851412982b9840d4fa42bd7563b622f6a
mozaikhty <ahmetdincr7@gmail.com>
2026-09-26 14:40:57 +0300

    Delete verify_profile.js

--- verify_profile.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('profile.js', 'utf8');
-const lines = code.split('\n');
-const idx = lines.findIndex(l => l.includes("document.getElementById('post-detail-container').innerHTML = html;"));
-console.log(lines.slice(idx - 2, idx + 2).join('\n'));
